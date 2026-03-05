bb517417a50ca792244a115cd6fdccb383733e92
Raushan <raushan.fullstack@gmail.com>
2026-03-05 15:51:53 +0530

    backend setup

--- backend_nestjs/src/modules/hello/hello.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('hello')
-export class HelloController {
-  @Get()
-  getHello(): { message: string } {
-    return { message: 'Hello, Ratan!' };
-  }
-}
