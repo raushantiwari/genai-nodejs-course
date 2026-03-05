c4882b067be942aaec65a010be1e5dad7610989a
Raushan <raushan.fullstack@gmail.com>
2026-03-05 13:50:43 +0530

    code upda

--- fe_rag_app_nextjs/components/ChatBoxInput/index.tsx
+++ fe_rag_app_nextjs/components/ChatBoxInput/index.tsx
@@ -1,17 +1,54 @@
+'use client';
+
+import { ICONS } from '@/utils/globalSvg';
+import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import styles from './ChatBoxInput.module.scss';
+
+type FormValues = {
+  message: string;
+};
+
 const ChatBoxInput = () => {
+  const { register, handleSubmit, reset } = useForm<FormValues>();
+
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data.message);
+    reset();
+  };
+
+  const autoResize = () => {
+    const textarea = textareaRef.current;
+    if (!textarea) return;
+
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+  };
+
   return (
-    <div className={styles['chat-input']}>
-      <div>
-        <button className={styles.plus}>+</button>
-      </div>
-      <div className={styles.inputbox}>
-        <input type="text" placeholder="Ask anything" />
-      </div>
-      <div>
-        <button className={styles.voice}>🎤</button>
-      </div>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles['chat-input']}>
+      <button type="button" className={styles.plus}>
+        {ICONS.PLUS_ICON}
+      </button>
+
+      <textarea
+        {...register('message')}
+        ref={(e) => {
+          register('message').ref(e);
+          textareaRef.current = e;
+        }}
+        rows={1}
+        placeholder="Ask anything"
+        className={styles.textarea}
+        onInput={autoResize}
+      />
+
+      <button type="submit" className={styles.voice}>
+        {ICONS.TOP_ARROW}
+      </button>
+    </form>
   );
 };
 
--- fe_rag_app_nextjs/utils/globalSvg.tsx
+++ fe_rag_app_nextjs/utils/globalSvg.tsx
@@ -89,4 +89,53 @@ export const ICONS = {
       />
     </svg>
   ),
+  PLUS_ICON: (
+    <svg
+      fill="#858585"
+      width="20px"
+      height="20px"
+      viewBox="0 0 32 32"
+      style={{
+        fillRule: 'evenodd',
+        clipRule: 'evenodd',
+        strokeLinejoin: 'round',
+        strokeMiterlimit: 2,
+      }}
+      version="1.1"
+      xmlSpace="preserve"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      stroke="#858585"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path d="M14.5,14.501l-10.502,0c-0.828,0 -1.5,0.673 -1.5,1.5c0,0.828 0.672,1.5 1.5,1.5l10.502,0l-0.001,10.502c0,0.828 0.672,1.5 1.5,1.501c0.828,-0 1.5,-0.673 1.5,-1.5l0.001,-10.503l10.502,0c0.828,0 1.5,-0.672 1.5,-1.5c0,-0.827 -0.672,-1.5 -1.5,-1.5l-10.502,0l0.001,-10.501c-0,-0.828 -0.672,-1.501 -1.5,-1.501c-0.828,0 -1.5,0.672 -1.5,1.5l-0.001,10.502Z"></path>
+        <g id="Icon"></g>
+      </g>
+    </svg>
+  ),
+
+  TOP_ARROW: (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#838181"
+      width={20}
+      height={20}
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M12 6V18M12 6L7 11M12 6L17 11"
+          stroke="#7f7a7a"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </g>
+    </svg>
+  ),
 };
